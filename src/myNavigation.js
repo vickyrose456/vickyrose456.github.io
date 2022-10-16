eab71506ef44d815fbef48b4df9cf04c7b26e747
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-10-15 23:47:28 -0400

    updating navigation background color

--- src/myNavigation.js
+++ src/myNavigation.js
@@ -4,7 +4,7 @@ template.innerHTML = `
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <link href="/style/style.css">
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding: 10px;">
+<nav class="navbar navbar-expand-lg navbar-light" style="padding: 10px; background-color: #e3f2fd;">
     <a class="navbar-brand" href="/index.html">
         <img src="/images/icon.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
         Victoria's brand
